e8eb9fe5a3cb6f80a494fd0d49210ab7ce5a0567
Jiro_Raiden <bishalmandi428@gmail.com>
2026-06-28 17:42:56 +0530

    Initialized backend database schema with Prisma

---
+++ backend/prisma/migrations/20260628112642_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "location" TEXT NOT NULL,
+    "deviceId" TEXT NOT NULL,
+    "merchantName" TEXT NOT NULL,
+    "flagged" BOOLEAN NOT NULL DEFAULT false,
+    "ruleReasons" TEXT[],
+    "mlScore" DOUBLE PRECISION,
+    "llmExplanation" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
